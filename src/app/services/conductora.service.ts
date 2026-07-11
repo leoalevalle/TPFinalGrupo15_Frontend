9aa45a707ff015f50ee39713a906e4a16d7f1a39
leoalevalle2014 <leoalevalle2014@gmail.com>
2026-07-11 02:24:59 -0300

    deployando

--- src/app/services/conductora.service.ts
+++ src/app/services/conductora.service.ts
@@ -8,8 +8,8 @@ import { Vehiculo } from '../models/vehiculo';
 })
 export class ConductoraService {
 
-  private api="http://localhost:3000/api";
-  // 'https://tpfinalgrupo15-backend.onrender.com/api'
+  //private api="http://localhost:3000/api";
+  private api = 'https://tpfinalgrupo15-backend.onrender.com/api';
   constructor(private http:HttpClient){} 
       
   iniciarJornada(data:any){
